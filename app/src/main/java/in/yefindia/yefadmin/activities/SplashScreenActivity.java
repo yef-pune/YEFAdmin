e76223b248df3f1af8a0d741390cd692aa095d0e
Pranav <pksharma7856@gmail.com>
2018-06-01 19:29:25 +0530

    Splash Screen

--- app/src/main/java/in/yefindia/yefadmin/activities/SplashScreenActivity.java
+++ app/src/main/java/in/yefindia/yefadmin/activities/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package in.yefindia.yefadmin.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,18 @@ import in.yefindia.yefadmin.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    public static final int SPLASH_SCREEN_TIME=3;   // Specifies time of splash screen in seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME * 1000);
     }
 }
